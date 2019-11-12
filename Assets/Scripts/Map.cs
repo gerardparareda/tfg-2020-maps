1e2046cf8a2482ffe16665c8cd006371f9eff4bb
Gerard Parareda Gallifa <gerard.parareda@students.salle.url.edu>
2019-11-12 17:17:38 +0100

    Creat projecte

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    float startPosX;
+    float startPosY;
+    public bool isBeingHeld = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHeld)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, -0.01f);
+        }        
+    }
+
+    private void OnMouseDown()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+
+    }
+
+    private void OnMouseUp()
+    {
+
+        isBeingHeld = false;
+    }
+}
